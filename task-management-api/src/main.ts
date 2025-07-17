23bb41856ad7b0b28d732066179833e0840fa7f1
unknown <pankajchavan451@gmail.comcd C:\Users\DELL\auth-cookie-appclaearclearclgit config --global user.email>
2025-07-17 11:23:54 +0530

    update main file

--- task-management-api/src/main.ts
+++ task-management-api/src/main.ts
@@ -1,30 +1,55 @@
- import { NestFactory } from '@nestjs/core';
- import { AppModule } from './app.module';
- import { ValidationPipe } from '@nestjs/common';
-import { RolesGuard } from './common/guards/roles.guard';
-import cookieParser from 'cookie-parser'
+//  import { NestFactory } from '@nestjs/core';
+//  import { AppModule } from './app.module';
+//  import { ValidationPipe } from '@nestjs/common';
+// import { RolesGuard } from './common/guards/roles.guard';
+// import cookieParser from 'cookie-parser'
 
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//  // app.use(cookieParser()); // Cookies pars
+//     app.use(cookieParser());
+
+//   app.enableCors({
+    
+   
+//      origin: [
+//   //  'http://localhost:3001',
+//   // 'https://my-auth-colwuype1-pankaj-chavans-projects-fc13a409.vercel.app'
+
+//     // 'http://localhost:3001',
+//     'https://my-auth-app-six.vercel.app',
+//     'https://my-auth-qkl173jkw-pankaj-chavans-projects-fc13a409.vercel.app', // 👈 हे नवं add करा
+// ],
+//     credentials: true,                
+//   });   
+//   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+//  //app.useGlobalGuards(app.get(JwtAuthGuard), app.get(RolesGuard));
+//   await app.listen(3000);
+// }
+// bootstrap();
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
- // app.use(cookieParser()); // Cookies pars
-    app.use(cookieParser());
+
+  app.use(cookieParser());
 
   app.enableCors({
-    
-   
-     origin: [
-  //  'http://localhost:3001',
-  // 'https://my-auth-colwuype1-pankaj-chavans-projects-fc13a409.vercel.app'
+    origin: [
+      'https://my-auth-app-six.vercel.app',
+      'https://my-auth-qkl173jkw-pankaj-chavans-projects-fc13a409.vercel.app',
+    ],
+    credentials: true,
+  });
 
-    // 'http://localhost:3001',
-    'https://my-auth-app-six.vercel.app',
-    'https://my-auth-qkl173jkw-pankaj-chavans-projects-fc13a409.vercel.app', // 👈 हे नवं add करा
-],
-    credentials: true,                
-  });   
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
- //app.useGlobalGuards(app.get(JwtAuthGuard), app.get(RolesGuard));
-  await app.listen(3000);
+
+  // ✅ Render compatible port setup
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
