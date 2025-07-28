c22a2c2b2bda9d3a8047975816418a1749e26321
unknown <pankajchavan451@gmail.comcd C:\Users\DELL\auth-cookie-appclaearclearclgit config --global user.email>
2025-07-28 09:49:42 +0530

    update

--- my-auth-app/src/api/api.js
+++ my-auth-app/src/api/api.js
@@ -3,7 +3,9 @@ import axios from "axios";
 
 const API = axios.create({
   //baseURL: process.env.REACT_APP_API_URL || "http://localhost:3000",
-    baseURL: 'https://task-management-api-f496.onrender.com',
+    //baseURL: 'https://task-management-api-f496.onrender.com',
+    baseURL: import.meta.env.VITE_API_URL,
+
 
   withCredentials: true,
 });
